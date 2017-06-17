50e69c0a73313f27646f8dc53c2427bd019a50aa
shreyaspadhy <shreyaspadhy@gmail.com>
2017-06-17 12:20:40 +0000

    svm v4

--- cs231n/gradient_check.py
+++ cs231n/gradient_check.py
@@ -123,5 +123,6 @@ def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
     grad_numerical = (fxph - fxmh) / (2 * h)
     grad_analytic = analytic_grad[ix]
     rel_error = abs(grad_numerical - grad_analytic) / (abs(grad_numerical) + abs(grad_analytic))
-    print('numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error))
+    #print('numerical: %f analytic: %f, relative error: %e' % (grad_numerical, grad_analytic, rel_error))
+    print('Relative Error : ', rel_error)
 
